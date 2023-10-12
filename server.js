695b27a342cae2c2272930a71313ceda0cbe74d3
Gokul Jayavel <jayavel.g@northeastern.edu>
2023-10-12 14:52:40 -0400

    updated readme

--- server.js
+++ server.js
@@ -8,19 +8,19 @@ config();
 
 
 
-const user = process.env.MYSQL_USER;
-const password = process.env.MYSQL_PASSWORD;
-const host = process.env.MYSQL_HOST;
-const port = process.env.MYSQL_PORT;
-const database = process.env.MYSQL_DATABASE;
-const sequelize = new Sequelize({
-  dialect: 'mysql', // Use the correct database dialect (e.g., 'mysql', 'postgres', etc.)
-  host: host,
-  username: user,
-  password: password,
-  database: database,
+// const user = process.env.MYSQL_USER;
+// const password = process.env.MYSQL_PASSWORD;
+// const host = process.env.MYSQL_HOST;
+// const port = process.env.MYSQL_PORT;
+// const database = process.env.MYSQL_DATABASE;
+// const sequelize = new Sequelize({
+//   dialect: 'mysql', // Use the correct database dialect (e.g., 'mysql', 'postgres', etc.)
+//   host: host,
+//   username: user,
+//   password: password,
+//   database: database,
 
-});
+// });
 
 sequelize.sync({ alter: true });
 
