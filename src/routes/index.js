c32c7a2706718775c1f47765c2a49680c2b8fa24
Gokul Jayavel <112915309+gokul-j99@users.noreply.github.com>
2023-11-05 22:07:17 -0500

    Assignment05 dev (#39)
    * added workflow and yaml
    
    * added workflow and packer
    
    * added yaml
    
    * added yaml
    
    * added yaml
    
    * added yaml
    
    * added yaml
    
    * modified pipwline
    
    * modified pipwline
    
    * modified pipwline
    
    * modified source image
    
    * modified source image
    
    * modified shell command
    
    * modified shell command
    
    * modified shell command
    
    * modified shell command
    
    * modified shell command
    
    * modified shell command
    
    * modified shell command
    
    * modified shell command
    
    * added users
    
    * added code
    
    * added extra pipeline
    
    * updated pipeline
    
    * added env in provisioner
    
    * added env file and password secret
    
    * added env file and password secret
    
    * added code
    
    * added users.csv
    
    * added users.csv with sudo
    
    * added users.csv with sudo
    
    * modified var
    
    * modified variables in packer
    
    * added provisioner
    
    * added 404
    
    * added 400 in delete
    
    * added readm.md
    
    * modified packer
    
    * modified packer
    
    * added service
    
    * added ls
    
    * added path
    
    * added path
    
    * added path
    
    * added sudo to mv to etc
    
    * modified packer
    
    * modified packer
    
    * modified packer
    
    * modified packer
    
    * modified service
    
    * modified service
    
    * added user and group
    
    * added paker var
    
    * adderd pak var
    
    * added code
    
    * modified paker fmt
    
    * added /opt
    
    * added /opt
    
    * added /opt
    
    * added /opt
    
    * added /opt
    
    * added /opt
    
    * added /opt
    
    * added /opt
    
    * added /opt
    
    * added /opt
    
    * added /opt
    
    * Update README.md
    
    * added
    
    * added code
    
    * added code
    
    * added cloudwatch
    
    * sdded meta
    
    * added / at last

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ import baseRouter from './healthRoutes.js';
 import assignmentRouter from './assignmentRoutes.js'
 import logger from '../../logger.js';
 export default(app) => {
+    app.use('/healthz',baseRouter)
+    app.use('/v1/assignments',assignmentRouter)
     app.use('/',(req, res) => {
         res.set('Cache-Control', 'no-cache');
         res.status(200).send("You have reached Webapp");
     })
-    app.use('/healthz',baseRouter)
-    app.use('/v1/assignments',assignmentRouter)
     app.use((req, res) => {
         res.set('Cache-Control', 'no-cache');
         res.status(404).end();
