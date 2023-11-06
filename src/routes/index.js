0013b23dd53e462fa47b21f1a29999acd51c88b1
Gokul Jayavel <jayavel.g@northeastern.edu>
2023-11-06 01:20:01 -0500

    added statsd

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import baseRouter from './healthRoutes.js';
 import assignmentRouter from './assignmentRoutes.js'
 import logger from '../../logger.js';
+import {statsdMiddleware}from '../../statsd.js'
 export default(app) => {
+    app.use(statsdMiddleware)
     app.use('/healthz',baseRouter)
     app.use('/v1/assignments',assignmentRouter)
     app.use('/',(req, res) => {
