542499c72f52bdbd2aad010d7d470ae3e0212ac2
Gokul Jayavel <jayavel.g@northeastern.edu>
2023-11-14 14:55:39 -0500

    added metadata

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,28 @@ import baseRouter from './healthRoutes.js';
 import assignmentRouter from './assignmentRoutes.js'
 import logger from '../../logger.js';
 import {statsdMiddleware}from '../../statsd.js'
+import axios from 'axios';
 export default(app) => {
     app.use(statsdMiddleware)
     app.use('/healthz',baseRouter)
     app.use('/v1/assignments',assignmentRouter)
-    app.use('/',(req, res) => {
-        res.set('Cache-Control', 'no-cache');
-        res.status(200).send("You have reached Webapp");
+    app.use('/',async (req, res) => {
+
+        try {
+            // You can extend this with more metadata fields if required
+            const localIPv4 = await axios.get('http://169.254.169.254/latest/meta-data/local-ipv4');
+            const publicIPv4 = await axios.get('http://169.254.169.254/latest/meta-data/public-ipv4');
+            let data =  {
+              localIPv4: localIPv4.data,
+              publicIPv4: publicIPv4.data,
+            };
+            res.json(data).status(200);
+
+          } catch (error) {
+            res.set('Cache-Control', 'no-cache');
+          res.status(200).send("You have reached Webapp");
+            console.error('Error fetching EC2 metadata:', error);
+          }
     })
     app.use((req, res) => {
         res.set('Cache-Control', 'no-cache');
